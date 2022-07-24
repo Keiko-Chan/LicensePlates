b537286a901498da93ea2641651606ea80f74801
Ekaterina <dudenko.ei@phystech.edu>
2022-07-24 20:33:25 +0300

    have some result

--- jaccrad.py
+++ jaccrad.py
@@ -1,85 +1,41 @@
-import cv2 as cv
-import numpy as np
 from pathlib import Path
 import base64
-import json
-import os
-import ssl
-import copy
-import http.client as httplib 
-import dataset_class as dat
+import sighthound as Si
+import dataset_class as Dat
 from sklearn.metrics import jaccard_score
+from scipy.spatial.distance import jaccard
 
-PATH_TO_DATA1 = Path('..', 'dataset1', 'SSIG-SegPlate', 'testing', 'Track01' )
-HEADERS = {"Content-type": "application/json", "X-Access-Token": "yHxNQWht4qeyrR1THlWTbY8XKvCB9gnI5Tut"}
-CONN = httplib.HTTPSConnection("dev.sighthoundapi.com", context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2))
+PATH_TO_DATA1 = Path('..', 'dataset1', 'SSIG-SegPlate', 'testing', 'Track23' )
 
-#input = np.array([1,0,0,1,1,1]) for exaple
-def jaccard(pred, true):
-	similarity = jaccard_score(pred, true)
-	distance = jaccard(pred, true)
-	return similarity, distance
+def jaccard_res(pred, true, index):
+	similarity = jaccard_score(pred, true, average="micro")
+	#distance = jaccard(pred, true)
+	print(index + 1, "similarity =", similarity)
+	return similarity
 	
-def sighthound(image_data):
-	params = json.dumps({"image": image_data})
-	CONN.request("POST", "/v1/recognition?objectType=licenseplate", params, HEADERS)
-	response = CONN.getresponse()
-	result = response.read()
-	return result
+def sign_average(data, points):
+	res = 0
+	for  sign_index in range(0, 7):
+		true = data.get_bin_matrix(sign_index)
+		pred = Si.get_bin_matrix(points, data, sign_index)
+		res = res + jaccard_res(pred, true, sign_index)
 	
-def find_point_in_res(start, sight_res, strlen):
-	i = copy.copy(start)
-	px = 0
-	py = 0
-	st = ''
-	indx = 0
-	
-	for j in sight_res[start:strlen]:
-		if j.isdigit():
-			st = st + j
-			indx = 1
-		else:
-			if indx == 1:
-				if py == 0:
-					indx = 0
-					py = int(st)
-					
-					st = ''
-				else:
-					px = int(st)
-					i = i + 1
-					break
-		i = i + 1
-	#print(i)
-					
-	return px, py, i
-
-def sigh_res_SSIG(sight_res):
-	p = np.zeros((4, 7, 2), int)			#4 points, 7 signes, 2 - x and y
-	i = sight_res.rfind("characters")
-	strlen = len(sight_res)
-
-	
-	for k in range(0, 7):
-		i = sight_res.find("vertices", i)
-		p[0][k][0], p[0][k][1], i = find_point_in_res(i, sight_res, strlen)
-		p[1][k][0], p[1][k][1], i = find_point_in_res(i, sight_res, strlen)
-		p[2][k][0], p[2][k][1], i = find_point_in_res(i, sight_res, strlen)
-		p[3][k][0], p[3][k][1], i = find_point_in_res(i, sight_res, strlen)
-		#print(p[2][k][0], p[2][k][1])
-	return p
+	res = res / 7
+	print("average =", res)
+	return res	
 	
 def main():
-	print('I am work')
-	image_data = base64.b64encode(open(Path(PATH_TO_DATA1, 'Track1[01].png'), "rb").read()).decode()
-	result = str(sighthound(image_data))
-	print("Detection Results = " + result )
+	print("in process")
+	image_data = base64.b64encode(open(Path(PATH_TO_DATA1, 'Track23[01].png'), "rb").read()).decode()
+	result = str(Si.sighthound(image_data))
+	#print("Detection Results = " + result )
 	
-	data = dat.Dataset('Track1[01].txt', 'Track1[01].png', PATH_TO_DATA1)
+	data = Dat.Dataset('Track23[01].txt', 'Track23[01].png', PATH_TO_DATA1)
 	data.read_txt_SSIG()
-	data.get_bin_matrix(1)
 	
-	points = sigh_res_SSIG(result)
+	points = Si.sigh_res_SSIG(result)
+	
+	sign_average(data, points)
 
 if __name__ == "__main__":
 	main()
