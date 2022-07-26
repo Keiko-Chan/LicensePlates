51464285eb02af79103b570c6cce9f15ee3fed80
Ekaterina <dudenko.ei@phystech.edu>
2022-07-27 01:22:28 +0300

    not only 7 signs

--- jaccrad.py
+++ jaccrad.py
@@ -6,7 +6,7 @@ from sklearn.metrics import jaccard_score
 from scipy.spatial.distance import jaccard
 
 PATH_TO_DATA1 = Path('..', 'dataset1', 'SSIG-SegPlate', 'testing', 'Track23' )
-PATH_TO_DATA2 = Path('..', 'dataset2', 'UFPR-ALPR dataset', 'training', 'track0001' )
+PATH_TO_DATA2 = Path('..', 'dataset2', 'UFPR-ALPR dataset', 'testing', 'track0091' )
 
 def jaccard_res(pred, true, index):
 	similarity = jaccard_score(pred, true, average="micro")
@@ -14,9 +14,9 @@ def jaccard_res(pred, true, index):
 	print(index + 1, "similarity =", similarity)
 	return similarity
 	
-def sign_average(data, points):
+def sign_average(data, points, number):
 	res = 0
-	for  sign_index in range(0, 7):
+	for  sign_index in range(0, number):
 		true = data.get_bin_matrix(sign_index)
 		pred = Si.get_bin_matrix(points, data, sign_index)
 		res = res + jaccard_res(pred, true, sign_index)
@@ -33,15 +33,15 @@ def calculate_IoU(name, dset, dpath):
 	data = Dat.Dataset(name + ".txt", name + ".png", dpath)
 	data.read_txt(dset)
 	
-	points = Si.sigh_res(result)
+	points = Si.read_sigh_res(result, 7)
 		
-	sign_average(data, points)
+	sign_average(data, points, 7)
 	
 def main():
 	print("in process...")
 	
-	#calculate_IoU("Track23[01]", "SSIG", PATH_TO_DATA1)
-	calculate_IoU("track0001[03]", "UFPR", PATH_TO_DATA2)
+	calculate_IoU("Track23[01]", "SSIG", PATH_TO_DATA1)
+	#calculate_IoU("track0091[01]", "UFPR", PATH_TO_DATA2)
 
 if __name__ == "__main__":
 	main()
