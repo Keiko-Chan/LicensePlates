838f838eca26e68ed647504fe43321d1e93a6902
Ekaterina <dudenko.ei@phystech.edu>
2022-07-30 22:45:37 +0300

    save points information

--- jaccrad.py
+++ jaccrad.py
@@ -5,9 +5,12 @@ import sighthound as Si
 import dataset_class as Dat
 from sklearn.metrics import jaccard_score
 from scipy.spatial.distance import jaccard
+import json
+import numpy as np
 
 PATH_TO_DATA1 = Path('..', 'dataset1', 'SSIG-SegPlate', 'testing', 'Track23' )
-PATH_TO_DATA2 = Path('..', 'dataset2', 'UFPR-ALPR dataset', 'testing', 'track0091' )		
+PATH_TO_DATA2 = Path('..', 'dataset2', 'UFPR-ALPR dataset', 'testing', 'track0091' )
+SIGNS_NUM = 7
 
 def jaccard_res(pred, true, index):
 	similarity = jaccard_score(pred, true, average="micro")
@@ -22,21 +25,40 @@ def sign_average(data, points, number):
 		pred = Si.get_bin_matrix(points, data, sign_index)
 		res = res + jaccard_res(pred, true, sign_index)
 	
-	res = res / 7
+	res = res / SIGNS_NUM
 	print("average =", res)
 	return res
 	
 def calculate_IoU_sight(name, dset, dpath):	
-	image_data = base64.b64encode(open(Path(dpath, name + ".png"), "rb").read()).decode()
-	result = str(Si.sighthound(image_data))
-	#print("Detection Results = " + result )
+
+	p_path = Path('sightound_points_' + dset)
+
+	if(p_path.exists() == False):
+		p_path.mkdir()
+	
+	p_path = Path(p_path, name + '.json')
+		
+	if(p_path.exists() == False):
+			
+		image_data = base64.b64encode(open(Path(dpath, name + ".png"), "rb").read()).decode()
+		result = str(Si.sighthound(image_data))
+		#print("Detection Results = " + result )
+		points = Si.read_sigh_res(result, SIGNS_NUM)
+			
+		with open(str(p_path), 'w') as f:
+			json.dump(points.tolist(), f)
+	else:
+		
+		with open(str(p_path)) as f:
+			file_content = f.read()
+			p_list = json.loads(file_content)
+			points = np.array(p_list)
+	
 	
 	data = Dat.Dataset(name + ".txt", name + ".png", dpath)
-	data.read_txt(dset)
-	
-	points = Si.read_sigh_res(result, 7)
+	data.read_txt(dset)	
 		
-	return sign_average(data, points, 7)
+	return sign_average(data, points, SIGNS_NUM)
 	
 	
 def dataset_IoU_sight(path, dset):
@@ -59,10 +81,10 @@ def dataset_IoU_sight(path, dset):
 def main():
 	print("in process...")
 	
-	#calculate_IoU_sight("Track23[01]", "SSIG", PATH_TO_DATA1)
-	#calculate_IoU_sight("track0091[01]", "UFPR", PATH_TO_DATA2)
+	calculate_IoU_sight("Track23[01]", "SSIG", PATH_TO_DATA1)
+	calculate_IoU_sight("track0091[01]", "UFPR", PATH_TO_DATA2)
 	
-	dataset_IoU_sight("../dataset1", "SSIG")
+	#dataset_IoU_sight("../dataset1", "SSIG")
 
 if __name__ == "__main__":
 	main()
