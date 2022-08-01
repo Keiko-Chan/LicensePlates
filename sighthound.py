2184914df8877be12dadfebc509bfda23f5cc91b
Ekaterina <dudenko.ei@phystech.edu>
2022-08-01 23:54:08 +0300

    separate saving and counting

--- sighthound.py
+++ sighthound.py
@@ -4,15 +4,38 @@ import json
 import os
 import ssl
 import numpy as np
+from pathlib import Path
+import base64
 
 HEADERS = {"Content-type": "application/json", "X-Access-Token": "yHxNQWht4qeyrR1THlWTbY8XKvCB9gnI5Tut"}
 CONN = httplib.HTTPSConnection("dev.sighthoundapi.com", context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2))
 
-def sighthound(image_data):
-	params = json.dumps({"image": image_data})
-	CONN.request("POST", "/v1/recognition?objectType=licenseplate", params, HEADERS)
-	response = CONN.getresponse()
-	result = response.read()
+def sighthound(dset, dpath, name):
+
+	res_path = Path('sightound_result_' + dset)
+	
+	if(res_path.exists() == False):
+		res_path.mkdir()
+		
+	res_path = Path(res_path, name + '.json')
+	
+	if(res_path.exists() == False):
+		image_data = base64.b64encode(open(Path(dpath, name + ".png"), "rb").read()).decode()
+
+		params = json.dumps({"image": image_data})
+		CONN.request("POST", "/v1/recognition?objectType=licenseplate", params, HEADERS)
+		response = CONN.getresponse()
+		result = response.read()
+		result = str(result)
+		
+		with open(str(res_path), 'w') as f:
+			json.dump(result, f)
+			
+	else:
+		
+		with open(str(res_path)) as f:
+			file_content = f.read()
+			result = json.loads(file_content)
 		
 	return result
 	
@@ -66,15 +89,16 @@ def get_lp_signs(sight_res):
 	else:
 		lp = sight_res[indx1:indx2]
 	#print('founded number =', lp)
-	return lp	
-
+	return lp		
 
 def read_sigh_res(sight_res, signes_num):				#signes_num = 7 for brazil
 	res_num = get_number_of_signs(sight_res)
 	
-	p = np.zeros((4, signes_num, 2), int)			#4 points, 7 signes, 2 - x and y
+	p = np.zeros((3, 2), int)					#4 points, 7 signes, 2 - x and y
+	sq = np.zeros((4, signes_num), int)				#X, Y, x, y
+	
 	if(signes_num != res_num):
-		return p
+		return sq
 
 	
 	i = sight_res.rfind("characters")
@@ -84,25 +108,31 @@ def read_sigh_res(sight_res, signes_num):				#signes_num = 7 for brazil
 	
 	for k in range(0, res_num):
 		i = sight_res.find("vertices", i)
-		p[0][k][0], p[0][k][1], i = find_point_in_res(i, sight_res, strlen)
-		p[1][k][0], p[1][k][1], i = find_point_in_res(i, sight_res, strlen)
-		p[2][k][0], p[2][k][1], i = find_point_in_res(i, sight_res, strlen)
-		p[3][k][0], p[3][k][1], i = find_point_in_res(i, sight_res, strlen)
-		#print(p[2][k][0], p[2][k][1])
-	return p
+		p[0][0], p[0][1], i = find_point_in_res(i, sight_res, strlen)
+		p[1][0], p[1][1], i = find_point_in_res(i, sight_res, strlen)
+		p[2][0], p[2][1], i = find_point_in_res(i, sight_res, strlen)
+		#p[3][k][0], p[3][k][1], i = find_point_in_res(i, sight_res, strlen)
+		
+		sq[1][k] = p[0][1]
+		sq[0][k] = p[0][0]
+		sq[3][k] = p[2][1] - p[0][1]
+		sq[2][k] = p[1][0] - p[0][0]		
 
-def get_bin_matrix(p, data, indx):
+	return sq
+
+
+def get_bin_matrix(sq, data, indx):
 	y_img, x_img = data.get_img_size()	
 	matrix = np.zeros((y_img, x_img))
 	
-	Y = p[0][indx][1]
-	X = p[0][indx][0]
-	y = p[2][indx][1] - p[0][indx][1]
-	x = p[1][indx][0] - p[0][indx][0]
+	X = sq[0][indx]
+	Y = sq[1][indx]
+	x = sq[2][indx]
+	y = sq[3][indx]
 		
 	for i in range(0, x):
 		for j in range(0, y):
-			matrix[y_img - 1 - Y + j][X + i] = 1	#may be error hear
+			matrix[y_img - 1 - Y + j][X + i] = 1	#may be error here
 		
 	#print(matrix)
 	return matrix
