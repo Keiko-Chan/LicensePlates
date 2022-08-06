ce045b28422dc4484ef16540d2bbf160e6300423
Ekaterina <dudenko.ei@phystech.edu>
2022-08-06 23:31:43 +0300

    errors of sighthound exist

--- sighthound.py
+++ sighthound.py
@@ -27,8 +27,8 @@ def sighthound(dset, dpath, name):
 		response = CONN.getresponse()
 		result = response.read()
 		result = json.loads(result)
-		obj = (result['objects'])[0]['licenseplateAnnotation']['attributes']['system']['characters'][0]['bounding']['vertices'][0]
-		print(obj)
+		#obj = (result['objects'])[0]['licenseplateAnnotation']['attributes']['system']['characters'][0]['bounding']['vertices'][0]
+		#print(obj)	
 		
 		with open(str(res_path), 'w') as f:
 			json.dump(result, f)
@@ -38,6 +38,15 @@ def sighthound(dset, dpath, name):
 		with open(str(res_path)) as f:
 			file_content = f.read()
 			result = json.loads(file_content)
+			
+	if 'error' in result:
+			if result['error'] == "ERROR_OVER_THROTTLE":
+				os.remove(res_path)
+				result = sighthound(dset, dpath, name)
+				
+			else:
+				print(name, "have an error:", result['error'])
+				return -1
 		
 	return result
 	
@@ -67,15 +76,23 @@ def get_lp_signs(sight_res):
 	return lp		
 
 def read_sigh_res(sight_res, signes_num):				#signes_num = 7 for brazil
-	obj = sight_res['objects'][0]['licenseplateAnnotation']['attributes']['system']['characters']			#[0]['bounding']['vertices'][0]
 
-	res_num = get_number_of_signs(str(obj))
-	
+	obj = sight_res['objects']
+	lp_num = 0
+	res_num = 0
+
+	for i in range(0, len(obj)):
+		res_num = get_number_of_signs(str(obj[i]))
+		if res_num == signes_num:
+			lp_num = i
+
 	rect = np.zeros((4, signes_num), int)				#X, Y, x, y
 	
 	if(signes_num != res_num):
 		return rect
-	
+
+	obj = obj[lp_num]['licenseplateAnnotation']['attributes']['system']['characters']			#[0]['bounding']['vertices'][0]
+
 	for k in range(0, signes_num):
 		rect[1][k] = obj[k]['bounding']['vertices'][0]['y']
 		rect[0][k] = obj[k]['bounding']['vertices'][0]['x']
