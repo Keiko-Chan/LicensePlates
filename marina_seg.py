8c847cafb4df33d61cceda3f0bb6ed65da3f8320
Ekaterina <dudenko.ei@phystech.edu>
2022-10-31 14:48:27 +0300

    fix

--- marina_seg.py
+++ marina_seg.py
@@ -9,6 +9,7 @@ import __init__ as In
 #------------------------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------------------------
 CPP_PROJ_LIB_PATH = "/home/ekatrina/prog/svn/trunk/build/lib.linux64.release"
+TEMPLATE_PATH = "/home/ekatrina/prog/svn/trunk/data/templates/brazilian"
 #------------------------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------------------------
 def get_marina_res(dset, name, img_format, path):
@@ -27,9 +28,22 @@ def get_marina_res(dset, name, img_format, path):
 	return result
 #------------------------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------------------------
-def proj_matrix(frame):
-	height = 130		#take it from template !!!
-	width = 400
+def proj_matrix(frame, moto):
+
+	if(moto == "motorcycle"):
+		template_path = Path(TEMPLATE_PATH, "brazilian_moto.json")
+	else:
+		template_path = Path(TEMPLATE_PATH, "brazilian.json")
+		
+	with open(str(template_path)) as f:
+			file_content = f.read()
+			template = json.loads(file_content)
+
+	#height = 130		#take it from template !!!
+	#width = 400
+	
+	height = int(template["size"][1])
+	width = int(template["size"][0])
 	
 	In.init(CPP_PROJ_LIB_PATH)
 	prj_matrix = In.calc_projective_matrix_from_rect_to_quad(frame, width, height)	
@@ -78,7 +92,7 @@ def founded(mar_res):
 	return False
 #------------------------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------------------------	
-def read_marina_res(mar_res, signes_num, lp_widht, lp_height, lp_x = 0, lp_y = 0):
+def read_marina_res(mar_res, signes_num, moto, lp_widht, lp_height, lp_x = 0, lp_y = 0):
 	obj = mar_res['plates']
 	lp_num = 0
 	
@@ -99,7 +113,7 @@ def read_marina_res(mar_res, signes_num, lp_widht, lp_height, lp_x = 0, lp_y = 0
 	else: 
 		return symbol_rectangles
 		
-	prj_matrix = proj_matrix(frame)
+	prj_matrix = proj_matrix(frame, moto)
 	new_prj_matrix = new_proj_matrix(frame, prj_matrix, lp_height, lp_widht)
 	
 	for k in range(0, signes_num):
@@ -116,7 +130,7 @@ def read_marina_res(mar_res, signes_num, lp_widht, lp_height, lp_x = 0, lp_y = 0
 	return symbol_rectangles
 #------------------------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------------------------
-def get_points(mar_res, signes_num):
+def get_points(mar_res, signes_num, moto):
 	obj = mar_res['plates']
 	lp_num = 0
 
@@ -127,6 +141,10 @@ def get_points(mar_res, signes_num):
 			lp_num = i
 			
 	points = np.zeros((signes_num, 4, 2), int)
+	
+	if(obj[lp_num]['rejected'] == True):
+		print("rejected")
+		return points
 
 	##if regected return -1 ?
 	
@@ -137,7 +155,7 @@ def get_points(mar_res, signes_num):
 	else: 
 		return points
 		
-	prj_matrix = proj_matrix(frame)
+	prj_matrix = proj_matrix(frame, moto)
 	
 	for k in range(0, signes_num):
 		point_4 = to_points_transform(obj[k], prj_matrix)
@@ -152,6 +170,8 @@ def get_binary_matrix(points_4, y_img, x_img):
 	matrix = np.zeros((y_img, x_img))
 	p = fig.Path([points_4[0], points_4[1], points_4[3], points_4[2]])
 	
+	#print(p)
+	
 	min_x = min(points_4[0][0], points_4[1][0], points_4[2][0], points_4[3][0])
 	max_x = max(points_4[0][0], points_4[1][0], points_4[2][0], points_4[3][0])
 	min_y = min(points_4[0][1], points_4[1][1], points_4[2][1], points_4[3][1])
@@ -159,8 +179,10 @@ def get_binary_matrix(points_4, y_img, x_img):
 	
 	for i in range(min_x - 1, max_x + 1):
 		for j in range(min_y - 1, max_y + 1):
- 			if(p.contains_points([(i, j)])):
- 				matrix[y_img - 1 - j][i] = 1
- 				
+			point = np.array([i, j])
+			if(p.contains_points([(i, j)]) or np.array_equal(point, points_4[0]) or np.array_equal(point, points_4[1]) or np.array_equal(point, points_4[2]) or np.array_equal(point, points_4[3])):
+				matrix[y_img - 1 - j][i] = 1
+	
+				
 	return matrix
  	
